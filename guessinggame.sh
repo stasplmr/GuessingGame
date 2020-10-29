aab758c9ff83bc75ec73bf0e69029260149f23ef
stasplmr <stanislav6121@gmail.com>
2020-10-29 21:20:02 +0200

    fixed guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,32 @@
-function start {
-	echo "Enter the number of files in the current directory:"
-	read number
-	files=$(ls -l |wc -l)
+function start { 
+    echo "Enter the number of files in the current directory:"
+    read guess_amount
+    if ! [[ $guess_amount =~ $re ]]
+    then
+	echo "Not a number!"
+	start
+    fi
 }
+
+function getAmount {
+    files=$(ls -l |wc -l)-1
+    echo $files
+}
+
+real_amount=$(getAmount)
+re='^[0-9]+$'
+
 start
-while [[ $number -ne $files ]]
+
+while [[ $guess_amount -ne $real_amount ]]
 do
-	if [[ $number -lt $files ]]
+	if [[ $guess_amount -lt $real_amount ]]
 	then
 		echo "Too low"
 	else
 		echo "Too high"
 	fi
-	start
+        start
 done
+
 echo "It's correct answer"
